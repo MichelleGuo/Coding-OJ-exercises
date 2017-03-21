a23225413b49304f713806f61f92f23406405e9f
MichelleGuo <michellegzy@163.com>
2017-03-21 15:53:16 +0800

    commit

--- src/leetcode/binaryTree.java
+++ src/leetcode/binaryTree.java
@@ -8,7 +8,9 @@ import java.util.Stack;
 
 /**
  * Created by ZeyingGuo on 2017/3/21.
+ *  94. Binary Tree Inorder Traversal
  * 145. Binary Tree Postorder Traversal
+ * 144. Binary Tree Preorder Traversal
  */
 
 public class binaryTree {
@@ -49,30 +51,38 @@ public class binaryTree {
         }
 
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root);
-
-        while(!stack.isEmpty()){
-            TreeNode temp = stack.peek();
-            if(temp.left==null && temp.right==null){
-                TreeNode pop = stack.pop();
-                result.add(pop.val);
+        TreeNode temp =  root;
+        while(true){
+            while (temp!=null){
+                stack.push(temp);
+                temp = temp.left;
             }
-            else{
-                if(temp.right!=null){
-                    stack.push(temp.right);
-                    temp.right=null;
-                }
-                if(temp.left!=null){
-                    stack.push(temp.left);
-                    temp.left=null;
-                }
+            if(stack.isEmpty()){
+                break;
+            }
+            temp = stack.pop();
+            result.add(temp.val);
+            temp = temp.right;
+        }
+        return result;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> result=new ArrayList<Integer>();
+        if(root==null) return result;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+            result.add(temp.val);
+            if(temp.right!=null){
+                stack.push(temp.right);
+            }
+            if(temp.left!=null){
+                stack.push(temp.left);
             }
         }
-
-
-
         return result;
-
     }
 
 }
